6065882ce68cbebe99fa306afec1399ce9af3323
Seth Isaacs <57905195+Action-Action@users.noreply.github.com>
2026-02-13 11:15:02 -0500

    Update SnipeFieldMap.ps1

--- SnipeFieldMap.ps1
+++ SnipeFieldMap.ps1
@@ -1,5 +1,6 @@
-# Replace URL with company Snipe-IT URL, keep /api.... and everything after
+# Replace SNIPEURL with company Snipe-IT URL, keep /api.... and everything after
 # Replace TOKEN with API Key
 # Replace c:\path\to\file with save location
 
-Invoke-RestMethod -Uri "URL/api/v1/fields" -Method Get -Headers @{"Authorization" = "Bearer TOKEN"; "Accept" = "application/json"} | Select-Object -ExpandProperty rows | Select-Object name, db_column_name | Export-Csv -Path "c:\path\to\file" -NoTypeInformation
+
+Invoke-RestMethod -Url "SNIPEURL/api/v1/fields" -Method Get -Headers @{"Authorization" = "Bearer TOKEN"; "Accept" = "application/json"} | Select-Object -ExpandProperty rows | Select-Object name, db_column_name | Export-Csv -Path "c:\path\to\file" -NoTypeInformation
