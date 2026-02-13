f7977bb0ad43f92efbcbc5918a384ffd12c5adfc
Seth Isaacs <57905195+Action-Action@users.noreply.github.com>
2026-02-13 11:02:12 -0500

    Add files via upload
    Single-line PowerShell command to pull all custom fields from Snipe-IT

---
+++ SnipeFieldMap.ps1
@@ -0,0 +1,5 @@
+# Replace URL with company Snipe-IT URL, keep /api.... and everything after
+# Replace TOKEN with API Key
+# Replace c:\path\to\file with save location
+
+Invoke-RestMethod -Uri "URL/api/v1/fields" -Method Get -Headers @{"Authorization" = "Bearer TOKEN"; "Accept" = "application/json"} | Select-Object -ExpandProperty rows | Select-Object name, db_column_name | Export-Csv -Path "c:\path\to\file" -NoTypeInformation
